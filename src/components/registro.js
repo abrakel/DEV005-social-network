603b5eef222c064a6d43e3768b1c256c0717564a
adrieli2307 <aearmijosq1104@gmail.com>
2023-04-24 17:10:41 -0500

    Funcionalidad botón de Google, inicio y registro

--- src/components/registro.js
+++ src/components/registro.js
@@ -15,33 +15,63 @@ function registro(navigateTo) {
   img.id = 'imagen';
   img.src = '../Img/logo.jpg';
 
+  /* ----------- Correo ---------------------*/
+  const mailLabel = document.createElement('label');
+  const mail = document.createElement('input');
+  mailLabel.textContent = 'Correo electrónico:';
+  mailLabel.setAttribute('for', 'mail');
+  mail.id = 'mail';
+  mail.placeholder = 'usuario@dominio.com';
+  mail.addEventListener('blur', () => {
+    const email = mail.value;
+    if (!email.endsWith('@gmail.com') && !email.endsWith('@hotmail.com')) {
+      alert('Introduzca una dirección de correo electrónico válida');
+      mail.value = '';
+    }
+  });
+  document.body.appendChild(mailLabel);
+  document.body.appendChild(mail);
+
+  /* ----------- Contraseña ---------------------*/
   const passwordLabel = document.createElement('label');
   const password = document.createElement('input');
-  passwordLabel.textContent = 'Password:';
+  passwordLabel.textContent = 'Contraseña:';
   passwordLabel.setAttribute('for', 'password');
   password.id = 'password';
+  password.minLength = 6;
   password.maxLength = 10;
   password.type = 'password';
   password.placeholder = 'Enter a password';
 
-  const mailLabel = document.createElement('label');
-  const mail = document.createElement('input');
-  mailLabel.textContent = 'Email:';
-  mailLabel.setAttribute('for', 'mail');
-  mail.id = 'mail';
-  mail.placeholder = 'Write your email';
-
+  /* ----------- Botón regreso ---------------------*/
   const buttonReturn = document.createElement('button');
   buttonReturn.textContent = 'Regresar';
   buttonReturn.addEventListener('click', () => {
     navigateTo('/');
   });
 
+  /* ----------- Botón de Registro ---------------------*/
   const register = document.createElement('button');
   register.id = 'regist';
   register.textContent = 'Registrarse';
   register.addEventListener('click', () => {
-    autenticacion();
+    autenticacion(mail.value, password.value)
+      .then((userCredential) => {
+        alert('El usuario se registro con exito');
+        // Signed in
+        const user = userCredential.user;
+        user.textContent = '';
+        // ...
+        document.getElementById('mail').value = '';
+        document.getElementById('password').value = '';
+      })
+      .catch((error) => {
+        alert(error);
+
+        document.getElementById('mail').value = '';
+        document.getElementById('password').value = '';
+      });
+
     console.log('si sirvo');
   });
 
@@ -50,10 +80,10 @@ function registro(navigateTo) {
   section.append(img, form1);
   form1.append(
     title,
-    passwordLabel,
-    password,
     mailLabel,
     mail,
+    passwordLabel,
+    password,
     register,
     buttonReturn,
   );
